43aa90dd068a8694411f99b0acd11d790d7aa3e7
drco <derricko271@gmail.com>
2025-03-23 02:02:26 +0000

    updated Like models

--- blog_api/likes/models.py
+++ blog_api/likes/models.py
@@ -7,14 +7,17 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class LikeDislike(models.Model):
-
-    class LikeDislike(models.TextChoices):
-        LIKE = 'L', _("Like")
-        DISLIKE = 'D', _('Dislike')
+    LIKE = "L"
+    DISLIKE = "D"
+    
+    TYPE_CHOICES = [
+        (LIKE, "Like"),
+        (DISLIKE, "Dislike"),
+    ]
 
     post = models.ForeignKey(BlogPost, verbose_name=_("Post"), on_delete=models.CASCADE)
     user = models.ForeignKey(User, verbose_name=_("User"), on_delete=models.CASCADE)
-    type = models.CharField(_("Rating"), max_length=1, choices=LikeDislike)
+    type = models.CharField(_("Rating"), max_length=1, choices=TYPE_CHOICES)
 
     class Meta:
         unique_together = ('post', 'user')
